efbb0920f58423256683170e6cf418e29d7052ad
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-04-22 10:41:43 +0100

    add RejectTeacherReason Eloquent model

---
+++ backend/app/Infrastructure/Persistence/Eloquent/Model/RejectTeacherReason.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Infrastructure\Persistence\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class RejectTeacherReason extends Model
+{
+    
+    protected $table = 'rejected_teacher_reasons';
+
+    
+    protected $fillable = [
+        'message',
+        'teacher_id',
+        'generated_report_id',
+    ];
+
+    
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'teacher_id');
+    }
+
+    
+    // public function generatedReport(): BelongsTo
+    // {
+    //     return $this->belongsTo(GeneratedReport::class, 'generated_report_id');
+    // }
+}
