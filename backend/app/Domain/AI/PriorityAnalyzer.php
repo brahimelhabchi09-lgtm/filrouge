fa4d7b6511d8a0ed3e3d4d7bb0ee236761e3df6e
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-04-27 21:14:34 +0100

    refactor: remove Domain and Infrastructure architectural layers

--- backend/app/Domain/AI/PriorityAnalyzer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Domain\AI;
-
-interface PriorityAnalyzer{
-    public function analyze(string $newMessage, array $generatedReports): mixed;
-}
--- backend/app/Domain/Meeting/Repository/MeetingRepository.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Domain\Meeting\Repository;
-
-interface MeetingRepository
-{
-
-}
