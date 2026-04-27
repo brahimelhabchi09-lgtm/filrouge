336aced83b689263d203e3707df6a52c6f89620d
brahimelhabchi09-lgtm <brahimelhabchi09@gmail.com>
2026-04-27 21:14:14 +0100

    refactor: remove Domain layer imports from AdminCreateUserController

--- backend/app/Http/Controllers/AdminCreateUserController.php
+++ backend/app/Http/Controllers/AdminCreateUserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Infrastructure\Persistence\Eloquent\Model\User;
+use App\Model\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
